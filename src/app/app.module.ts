bf30ae4e4e0c2573343fbef73812344947ee1866
NicoleS <hjjsentis@MacBook-NicoleS.local>
2017-08-08 11:45:41 +0100

    created components for artists, artist-list, artist-detail, artist & artworks, artwork-list, artwork-detail, artwork-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ArtworksComponent } from './artworks/artworks.component';
+import { ArtworkListComponent } from './artworks/artwork-list/artwork-list.component';
+import { ArtworkDetailComponent } from './artworks/artwork-detail/artwork-detail.component';
+import { ArtworkItemComponent } from './artworks/artwork-list/artwork-item/artwork-item.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { ArtistListComponent } from './artists/artist-list/artist-list.component';
+import { ArtistComponent } from './artists/artist-list/artist/artist.component';
+import { ArtistDetailComponent } from './artists/artist-detail/artist-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArtworksComponent,
+    ArtworkListComponent,
+    ArtworkDetailComponent,
+    ArtworkItemComponent,
+    ArtistsComponent,
+    ArtistListComponent,
+    ArtistComponent,
+    ArtistDetailComponent
   ],
   imports: [
     BrowserModule
